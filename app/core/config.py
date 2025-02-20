3edd3272a23a7534a0ce2af9924d22de098bd396
ybn1247 <ybn1247@naver.com>
2025-02-20 15:52:05 +0900

    db test

--- app/core/config.py
+++ app/core/config.py
@@ -5,13 +5,13 @@ load_dotenv()
 
 
 class Settings:
-    DB_USERNAME: str = os.getenv("DB_USERNAME")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: str = os.getenv("DB_PORT")
-    DB_DATABASE: str = os.getenv("DB_DATABASE")
+    # DB_USERNAME: str = os.getenv("DB_USERNAME")
+    # DB_PASSWORD = os.getenv("DB_PASSWORD")
+    # DB_HOST: str = os.getenv("DB_HOST")
+    # DB_PORT: str = os.getenv("DB_PORT")
+    # DB_DATABASE: str = os.getenv("DB_DATABASE")
 
-    DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
-
+    # DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
 settings = Settings()
