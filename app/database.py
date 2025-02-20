125b1819267ab8cb3c8bb746e63970b2fe3f8803
ybn1247 <ybn1247@naver.com>
2025-02-20 15:46:19 +0900

    db 설정

--- app/database.py
+++ app/database.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 환경 변수에서 DB 정보 가져오기
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-
+# DB_USER = os.getenv("DB_USER")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_NAME = os.getenv("DB_NAME")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = os.getenv("DB_PORT")
+DATABASE_URL = os.getenv("DATABASE_URL")
 # DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-DATABASE_URL= "mysql+pymysql://root:root@mariadb:3306/krampoline"
+# DATABASE_URL= "mysql+pymysql://root:root@mariadb:3306/krampoline"
 
 # DB 엔진 생성
 engine = create_engine(DATABASE_URL)
