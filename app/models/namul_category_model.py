bb3f4d56990ee76a731de50c3d61a67162d55190
Su-Yeo <yyy1247@gmail.com>
2025-02-21 05:56:46 +0900

    [Fix] data

--- app/models/namul_category_model.py
+++ app/models/namul_category_model.py
@@ -6,7 +6,7 @@ class NamulCategory(Base):
     __tablename__ = "namul_category"
 
     id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
-    name = Column(String(100), nullable=False, unique=True) 
-    code = Column(String(100), nullable=False, unique=True)
+    name = Column(String(100), nullable=False) 
+    code = Column(String(100), nullable=False)
 
     namul = relationship('Namul', back_populates='category')
--- app/service/namul_category_service.py
+++ app/service/namul_category_service.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Session
 from app.models.namul_category_model import NamulCategory
 from app.views.namul_category_schema import NamulCategoryBase
+from sqlalchemy import asc
 
 def get_namul_category(db: Session):
-    return db.query(NamulCategory).all()
+    return db.query(NamulCategory).order_by(asc(NamulCategory.id)).all()
 
 def create_namul_category(db: Session, namul: NamulCategoryBase):
     db_namul_category = NamulCategory(name=namul.name, code=namul.code)
