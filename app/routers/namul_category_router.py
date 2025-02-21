e30b454bb342298e4a4fca0d3274896849fd1259
ybn1247 <ybn1247@naver.com>
2025-02-21 13:01:08 +0900

    [Test] 임시 데이터

--- app/routers/namul_category_router.py
+++ app/routers/namul_category_router.py
@@ -11,7 +11,46 @@ router = APIRouter(
 
 @router.post("/", response_model=namul_category_schema.NamulCategoryResponse)
 def create_namul_category(namul_category: namul_category_schema.NamulCategoryBase, db: Session = Depends(get_db)):
-    return namul_category_service.create_namul_category(db, namul_category)
+    employee_string = """[
+  {
+    "name": "고사리",
+    "code": "nav-gosari.svg",
+    "id": 1
+  },
+  {
+    "name": "두릅",
+    "code": "nav-durup.svg",
+    "id": 2
+  },
+  {
+    "name": "꿩마농",
+    "code": "nav-kkungmanung.svg",
+    "id": 3
+  },
+  {
+    "name": "취나물",
+    "code": "nav-gosari.svg",
+    "id": 4
+  },
+  {
+    "name": "냉이",
+    "code": "nav-gosari.svg",
+    "id": 5
+  },
+  {
+    "name": "근거리",
+    "code": "",
+    "id": 6
+  },
+  {
+    "name": "찜",
+    "code": "",
+    "id": 7
+  }
+]"""
+    json_object = json.loads(employee_string)
+    return json_object
+    # return namul_category_service.create_namul_category(db, namul_category)
 
 @router.get("/", response_model=list[namul_category_schema.NamulCategoryResponse])
 def read_namul_category(db: Session = Depends(get_db)):
