041a4d701c452965d40e238c6e60443022710b22
Su-Yeo <yyy1247@gmail.com>
2025-02-21 14:15:33 +0900

    [Chore] import json

--- app/routers/namul_category_router.py
+++ app/routers/namul_category_router.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.views import namul_category_schema
 from app.service import namul_category_service
+import json
 
 router = APIRouter(
     prefix="/namuls/category",
