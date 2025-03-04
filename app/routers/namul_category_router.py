29a2a22069d5313f26518b592409a50b2cb7ec40
Su-Yeo <yyy1247@gmail.com>
2025-03-04 16:04:00 +0900

    test: return json data

--- app/routers/namul_category_router.py
+++ app/routers/namul_category_router.py
@@ -13,44 +13,43 @@ router = APIRouter(
 @router.post("/", response_model=namul_category_schema.NamulCategoryResponse)
 def create_namul_category(namul_category: namul_category_schema.NamulCategoryBase, db: Session = Depends(get_db)):
     employee_string = """[
-  {
-    "name": "고사리",
-    "code": "nav-gosari.svg",
-    "id": 1
-  },
-  {
-    "name": "두릅",
-    "code": "nav-durup.svg",
-    "id": 2
-  },
-  {
-    "name": "꿩마농",
-    "code": "nav-kkungmanung.svg",
-    "id": 3
-  },
-  {
-    "name": "취나물",
-    "code": "nav-gosari.svg",
-    "id": 4
-  },
-  {
-    "name": "냉이",
-    "code": "nav-gosari.svg",
-    "id": 5
-  },
-  {
-    "name": "근거리",
-    "code": "",
-    "id": 6
-  },
-  {
-    "name": "찜",
-    "code": "",
-    "id": 7
-  }
-]"""
-    json_object = json.loads(employee_string)
-    return json_object
+    {
+      "name": "고사리",
+      "code": "nav-gosari.svg",
+      "id": 1
+    },
+    {
+      "name": "두릅",
+      "code": "nav-durup.svg",
+      "id": 2
+    },
+    {
+      "name": "꿩마농",
+      "code": "nav-kkungmanung.svg",
+      "id": 3
+    },
+    {
+      "name": "취나물",
+      "code": "nav-gosari.svg",
+      "id": 4
+    },
+    {
+      "name": "냉이",
+      "code": "nav-gosari.svg",
+      "id": 5
+    },
+    {
+      "name": "근거리",
+      "code": "",
+      "id": 6
+    },
+    {
+      "name": "찜",
+      "code": "",
+      "id": 7
+    }
+    ]"""
+    return json.loads(employee_string)
     # return namul_category_service.create_namul_category(db, namul_category)
 
 @router.get("/", response_model=list[namul_category_schema.NamulCategoryResponse])
